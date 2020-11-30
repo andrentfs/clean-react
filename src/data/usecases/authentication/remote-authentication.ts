bb236323972382e8009914755708695a9a552fe0
Andre Luiz <andre@awtecnologia.com.br>
2020-11-30 05:30:04 +0000

    refactor: error my docker installed, save last config

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,6 +1,5 @@
-import { HttpPostClient } from '@/data/protocols/http/http-post-client'
+import { HttpPostClient, HttpStatusCode } from '@/data/protocols/http'
 import { AuthenticationParams, Authentication } from '@/domain/usecases/authentication'
-import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/error/invalid-credentials-error'
 import { UnexpectedError } from '@/domain/error/unexpected-error'
 import { AccountModel } from '@/domain/models/account-model'
